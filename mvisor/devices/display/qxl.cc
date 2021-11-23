1a8a71e5a5ef2a68abd7cd50fba252ae628c91a0
Xiaoxia <terrence.huang@tenclass.com>
2021-11-23 08:41:44 +0800

    Add GPL 3 License

--- mvisor/devices/display/qxl.cc
+++ mvisor/devices/display/qxl.cc
@@ -1,6 +1,23 @@
+/* 
+ * MVisor
+ * Copyright (C) 2021 Terrence <terrence@tenclass.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 /*
  * Since we want to implement QXL, so don't waste time on VGA
- * terrence.huang@tenclass.com
  */
 
 #include <cstring>
