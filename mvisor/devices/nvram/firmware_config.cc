226848a520f029efde6e800800fb03705830824d
Xiaoxia <terrence.huang@tenclass.com>
2022-02-10 07:09:19 +0800

    Automatically show boot menu

--- mvisor/devices/nvram/firmware_config.cc
+++ mvisor/devices/nvram/firmware_config.cc
@@ -114,14 +114,21 @@ class FirmwareConfig : public Device {
     SetConfigUInt32(FW_CFG_ID, version);
     SetConfigUInt32(FW_CFG_FILE_DIR, 0);
 
-    int num_vcpus = manager_->machine()->num_vcpus();
+    auto machine = manager_->machine();
+    int num_vcpus = machine->num_vcpus();
     SetConfigUInt16(FW_CFG_NB_CPUS, num_vcpus);
     SetConfigUInt16(FW_CFG_MAX_CPUS, num_vcpus);
     uint64_t numa_cfg[num_vcpus + 1] = { 0 };
     SetConfigBytes(FW_CFG_NUMA, std::string((const char*)numa_cfg, sizeof(numa_cfg)));
     SetConfigUInt16(FW_CFG_NOGRAPHIC, 0);
     SetConfigUInt32(FW_CFG_IRQ0_OVERRIDE, 1);
-    SetConfigUInt16(FW_CFG_BOOT_MENU, 2); // show menu if more than 1 drives
+
+    auto cdrom = machine->LookupObjectByClass("AhciCdrom");
+    if (cdrom && cdrom->has_key("image")) {
+      SetConfigUInt16(FW_CFG_BOOT_MENU, 2); // show menu if more than 1 drives
+    } else {
+      SetConfigUInt16(FW_CFG_BOOT_MENU, 0);
+    }
 
     InitializeE820Table();
 
